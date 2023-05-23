6a2b281116e191c5caeab7a7c54f463222a1e931
afk47 <bigandrewmarshall@gmail.com>
2023-05-22 20:29:22 -0400

    Model loading + Orthographic camera

--- core/src/com/marshall/benjy/qld/core/level/LevelRenderer.java
+++ core/src/com/marshall/benjy/qld/core/level/LevelRenderer.java
@@ -13,48 +13,51 @@ import java.util.List;
 
 public class LevelRenderer {
 
-    private float SCALE = 3f;
+	private float SCALE = 10f;
 
-    private Level level;
+	private Level level;
 
-    public List<ModelInstance> instances = new ArrayList<>();
-    private TileRenderer tileRenderer;
+	
+	public List<ModelInstance> instances = new ArrayList<>();
+	private TileRenderer tileRenderer;
 
-    public LevelRenderer(Level level, AssetManager assetManager) {
-        this.level = level;
-        this.tileRenderer = new TileRenderer(assetManager);
-    }
+	public LevelRenderer(Level level, AssetManager assetManager) {
+		this.level = level;
+		this.tileRenderer = new TileRenderer(assetManager);
+		
+	}
 
-    public void updateInstances() {
-        instances.clear();
-        for (int x = 0; x < level.getTiles().length; x++ ) {
-            for (int z = 0; z < level.getTiles()[x].length; z++) {
-                Tile workingTile = level.getTiles()[x][z];
-                Model tileModel = tileRenderer.getTileModel(workingTile);
-                if(tileModel == null) {
-                    System.out.println("Tile model not loaded, aborting draw");
-                    return;
-                }
-                ModelInstance tileInstance = new ModelInstance(tileModel);
-                tileInstance.transform.setToTranslation(x * SCALE, 0, z * SCALE);
-                instances.add(tileInstance);
-            }
-        }
-    }
+	public void updateInstances() {
+		instances.clear();
+		for (int x = 0; x < level.getTiles().length; x++) {
+			for (int z = 0; z < level.getTiles()[x].length; z++) {
+				Tile workingTile = level.getTiles()[x][z];
+				Model tileModel = tileRenderer.getTileModel(workingTile);
+				if (tileModel == null) {
+					System.out.println("Tile model not loaded, aborting draw");
+					return;
+				}
+				ModelInstance tileInstance = new ModelInstance(tileModel);
+				tileInstance.transform.setToTranslation(x * SCALE, 0, z * SCALE);
+				instances.add(tileInstance);
+			}
+		}
+	}
 
-    public List<ModelInstance> getInstances() {
-        if (!instances.isEmpty()) {
-            return instances;
-        }
+	public List<ModelInstance> getInstances() {
+		
+		if (!instances.isEmpty()) {
+			return instances;
+		}
 
-        try {
-            updateInstances();
-            return instances;
-        } catch (Exception ex) {
-            System.out.println(ex);
-            instances.clear();
-            return Collections.emptyList();
-        }
-    }
+		try {
+			updateInstances();
+			return instances;
+		} catch (Exception ex) {
+			System.out.println(ex);
+			instances.clear();
+			return Collections.emptyList();
+		}
+	}
 
 }
